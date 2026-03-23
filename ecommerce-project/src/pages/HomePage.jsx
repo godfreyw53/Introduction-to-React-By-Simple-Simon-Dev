424ef7b2f99ae18d8d7a9c309d1ba5a3405420b1
Godfrey Liuvmbazi <godfreyw54@gmail.com>
2026-03-23 13:20:01 +0300

    use data from the backend to generate html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
-import axios from 'axios'
+import axios from "axios";
+import { useEffect, useState} from "react";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
+//import { products } from "../../starting-code/data/products";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 export function HomePage() {
- axios.get('http://localhost:3000/api/products')
-  .then((response)=>{
-    console.log(response.data)
-  })
+  const [products, setProducts]=useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products")
+      .then((response) => {
+     setProducts(response.data);
+    });
+  },[]);
+
   return (
     <>
       <title>Ecommerce-Project</title>
@@ -19,10 +24,7 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -32,7 +34,7 @@ export function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars*10}.png`}
+                    src={`images/ratings/rating-${product.rating.stars * 10}.png`}
                   />
                   <div className="product-rating-count link-primary">
                     {product.rating.count}
@@ -40,7 +42,7 @@ export function HomePage() {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents/100).toFixed(2)}
+                  ${(product.priceCents / 100).toFixed(2)}
                 </div>
 
                 <div className="product-quantity-container">
